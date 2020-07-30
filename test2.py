f0ed60e8dba9fa07469e9a7bfc9255113f7f0cdc
Adam <36452622+adamdrucker@users.noreply.github.com>
2020-07-30 10:44:15 -0400

    Add files via upload

--- test2.py
+++ test2.py
@@ -1,47 +1,95 @@
 import time
 import cryptography
+from cryptography.fernet import Fernet
 from Crypto import Random
 from Crypto.Cipher import AES
 from Crypto.Cipher import Blowfish
 from Crypto.Hash import MD5
 
 
-
-# Create a new instance of the hash object
-hash = MD5.new()
-# Encode a message for hashing
-message = "Hashing".encode()
-# The 'update' method continues the hashing by consuming the next chunk
-hash.update(message)
-# The 'digest' method returns a digest of what's been hashed so far
-digest = hash.digest()
-print("MD5 digest: %s" % digest)
+def decorate():
+    print("=" * 34)
 
 
-# Create new AES cipher
-# An IV can be used, but 'MODE_ECB' ignores it
-obj = AES.new('This is a key456This is a key456',AES.MODE_ECB)
-messages = "The answer is no"
-ciphertext = obj.encrypt(messages)
-print("AES encryption: %s" % ciphertext)
-time.sleep(1)
-print("Decrypting AES...")
-time.sleep(1)
-# This decrypts and decodes
-aes_decrypt = obj.decrypt(ciphertext).decode()
-print(aes_decrypt)
+def md5_hash():
+    # Create a new instance of the hash object
+    hash = MD5.new()
+    # Encode a message for hashing
+    message = "Hashing".encode()
+    # The 'update' method continues the hashing by consuming the next chunk
+    hash.update(message)
+    # The 'digest' method returns a digest of what's been hashed so far
+    digest = hash.digest()
+    print("MD5 digest: %s" % digest)
+    decorate()
+md5_hash()
+
+# -----------
+
+def aes_encrypt():
+    # Create new AES cipher
+    # An IV can be used, but 'MODE_ECB' ignores it
+    iv = Random.new().read(AES.block_size)
+    obj = AES.new('This is a key456This is a key456',AES.MODE_CBC, iv)
+    message = "The answer is no"
+    ciphertext = obj.encrypt(message)
+    
+    print("AES encryption: %s" % ciphertext)
+    time.sleep(1)
+    print("Decrypting AES...")
+    time.sleep(1)
+    
+    # This decrypts and decodes
+    # Per pypi.org documentation,a second object is needed in order
+    # to properly decrypt (apparently)
+    obj2 = AES.new('This is a key456This is a key456',AES.MODE_CBC, iv)
+    # The decode() method below converts from byte string to UTF8
+    aes_decrypt = obj2.decrypt(ciphertext).decode('utf-8')
+    print(aes_decrypt)
+    decorate()
+aes_encrypt()
+
+# -----------
+
+# Encrypt works fine, decrypt coming out in bytes
+def blowfish_encrypt():
+    # This prints a Blowfish encrypted message
+    bs = Blowfish.block_size    # 8 bits
+    key = "An abritrarily long key"
+    iv = Random.new().read(bs)
+    obj = Blowfish.new(key, Blowfish.MODE_CBC, iv)
+    message = b"What the"
+    ciphertext = obj.encrypt(message)
+    
+    print("Blowfish encryption: %s" % ciphertext)
+    time.sleep(1)
+    print("Decrypting Blowfish...")
+    
+    # Not working, printing byte string
+    dec = obj.decrypt(ciphertext)
+    #plaintext = dec.decode()
+    print(dec)
+    decorate()
+blowfish_encrypt()
+
+# -----------
+
+# This works fine for in/out crypto
+def fernet_encryption():
+    # Encrypt with cryptography.fernet
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    message = b"Poo poo potty"
+    token = f.encrypt(message)
+    print("Fernet encryption: %s" % token)
+    dec = f.decrypt(token)
+    plaintext = dec.decode('utf8')
+    print("Decrypting Fernet...")
+    time.sleep(1)
+    print(plaintext)
+    decorate()
+fernet_encryption()
 
 
-# This prints a Blowfish encrypted message
-bs = Blowfish.block_size    # 8 bits
-iv = Random.new().read(bs)
-key = "An abritrarily long key"
-bobj = Blowfish.new(key, Blowfish.MODE_CBC, iv)
-bmessage = "What the".encode()
-cblow = bobj.encrypt(bmessage)
-print("Blowfish encryption: %s" % cblow)
-time.sleep(1)
-print("Decrypting Blowfish...")
-# Not working, printing byte string
-blow_decrypt = bobj.decrypt(cblow)
-print(blow_decrypt)
+
+    

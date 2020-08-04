3e3f00df9af9bfcc62b23f2dca2196a2d97e8058
Adam <36452622+adamdrucker@users.noreply.github.com>
2020-08-04 12:22:49 -0400

    Add files via upload

--- test2.py
+++ test2.py
@@ -12,6 +12,9 @@ from Crypto.Hash import MD5
     to be a multiple of the block size for each algorithm. I need to figure
     out how to ensure that each of these functions will perform the prescribed
     encryption on data of any length. (padding?)
+
+    CFB mode, described in a comment under the AES section, seems to have resolved
+    this issue for now.
 '''
 
 
@@ -21,21 +24,28 @@ def decorate():
 # -----------
 
 def md5_hash():
+    
     # Create a new instance of the hash object
     hash = MD5.new()
+    
     # Encode a message for hashing
     message = "Hashing".encode()
+    
     # The 'update' method continues the hashing by consuming the next chunk
     hash.update(message)
+    
     # The 'digest' method returns a digest of what's been hashed so far
     digest = hash.digest()
+    
     print("MD5 digest: %s" % digest)
-    decorate()
+    
+    decorate()  # Spacing for test output
+    
 md5_hash()
 
 # -----------
 
-''' The current AES encryption mode requires the inpur string to be a multiple
+''' The current AES encryption mode requires the input string to be a multiple
     of 16 in length -- need to figure out how to ensure that a string of
     anything length is processed in chunks until completion
 '''
@@ -44,8 +54,12 @@ def aes_encrypt():
     # Create new AES cipher
     # An IV can be used, but 'MODE_ECB' ignores it
     iv = Random.new().read(AES.block_size)
-    obj = AES.new('This is a key456This is a key456',AES.MODE_CBC, iv)
-    message = "The answer is no"
+    # AES.MODE_CFB seems to mitigate the issue with input/plaintext length
+    # CFB is "Cipher FeedBack" - this turns the block cipher into a stream cipher
+    obj = AES.new('This is a key456This is a key456',AES.MODE_CFB, iv)
+    
+    message = "Testing a string of an arbitrary length"
+    
     ciphertext = obj.encrypt(message)
     
     print("AES encryption: %s" % ciphertext)
@@ -55,12 +69,16 @@ def aes_encrypt():
     
     # This decrypts and decodes
     # Per pypi.org documentation,a second object is needed in order
-    # to properly decrypt (apparently)
-    obj2 = AES.new('This is a key456This is a key456',AES.MODE_CBC, iv)
+    # to properly decrypt
+    obj2 = AES.new('This is a key456This is a key456',AES.MODE_CFB, iv)
+    
     # The decode() method below converts from byte string to UTF8
     aes_decrypt = obj2.decrypt(ciphertext).decode('utf-8')
+    
     print(aes_decrypt)
-    decorate()
+    
+    decorate()  # Spacing for test output
+    
 aes_encrypt()
 
 # -----------
@@ -72,12 +90,20 @@ aes_encrypt()
 
 # Encrypt works fine, decrypt coming out in bytes
 def blowfish_encrypt():
+    
     # This prints a Blowfish encrypted message
+    
     bs = Blowfish.block_size    # 8 bits
+    
     key = "An arbitrarily long key"
+    
     iv = Random.new().read(bs)
-    obj = Blowfish.new(key, Blowfish.MODE_CBC, iv)
-    message = b"What the"
+    # CFB mode works identical to the AES change made above
+    # This appears to allow for plaintext input of any length
+    obj = Blowfish.new(key, Blowfish.MODE_CFB, iv)
+    
+    message = b"What the hell is going on"
+    
     ciphertext = obj.encrypt(message)
     
     print("Blowfish encryption: %s" % ciphertext)
@@ -86,21 +112,28 @@ def blowfish_encrypt():
     time.sleep(1)
     
     # Not working, printing byte string
-    obj2 = Blowfish.new(key, Blowfish.MODE_CBC, iv)
+    obj2 = Blowfish.new(key, Blowfish.MODE_CFB, iv)
     dec = obj2.decrypt(ciphertext).decode('utf-8')
     #plaintext = dec.decode()
+
     print(dec)
-    decorate()
+    
+    decorate()  # Spacing for test output
+    
 blowfish_encrypt()
 
 # -----------
 
 # This works fine for in/out crypto
 def fernet_encryption():
+    
     # Encrypt with cryptography.fernet
+    
     key = Fernet.generate_key()
     f = Fernet(key)
+    
     message = b"Poo poo potty"
+    
     token = f.encrypt(message)
     
     print("Fernet encryption: %s" % token)
@@ -110,8 +143,11 @@ def fernet_encryption():
     
     dec = f.decrypt(token)
     plaintext = dec.decode('utf8')
+    
     print(plaintext)
-    decorate()
+    
+    decorate()  # Spacing for test output
+    
 fernet_encryption()
 
 
